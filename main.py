665a5b2fe5a4effad89bd6a83dd8290c1bdb0a42
chokomilk07 <106243223+chokomilk07@users.noreply.github.com>
2024-10-30 19:24:13 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -4,23 +4,44 @@ import streamlit
 
 import streamlit as st
 
-st.write("Hello World")
+# st.write("Hello World")
 
-st.title('This is a title')
-st.header('This is a header')
-st.subheader('This is a subheader')
+# st.title('This is a title')
+# st.header('This is a header')
+# st.subheader('This is a subheader')
 
-st.text('Hello World. Streamlit text')
-name = 'Hong gildong'
-st.text('Hi, {}'.format(name))
+# st.text('Hello World. Streamlit text')
+# name = 'Hong gildong'
+# st.text('Hi, {}'.format(name))
 
-st.markdown('## This is markdown')
+# st.markdown('## This is markdown')
 
 
-hello = st.button("Hello")
-bye=st.button("Bye")
+# hello = st.button("Hello")
+# bye=st.button("Bye")
 
-if bye:
-    st.write("안녕히가세요")
-else:
-    st.write("안녕하세요.")
+# if bye:
+#     st.write("안녕히가세요")
+# else:
+#     st.write("안녕하세요.")
+
+st.sidebar.selectbox("MENU",["로그인","회원가입"])
+
+db_id="test"
+db_pw="123"
+
+
+st.title("로그인")
+
+
+id=st.text_input("아이디",placeholder="아이디를 입력하세요")
+pw=st.text_input("비밀번호",type="password")
+login=st.button("로그인")
+
+
+if login:
+    if db_id==id and db_pw==pw:
+        st.success("로그인 성공")
+        st.balloons()
+    else:
+        st.error("로그인 실패")
